5328d7bb580c6c2de3a635c15c8cafc09d5c5e76
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-11 10:23:00 -0500

    updated promises.js with new github token

--- js/promises.js
+++ js/promises.js
@@ -33,14 +33,29 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
 
-function acceptsGitHubUserName(){
+let githubAccessToken = localStorage.getItem("githubAccessToken")
 
-    return fetch('https://api.github.com/users/jameskevinayoung/events/public', {
-        headers: {'Authorization': '3abecaf9fc1d0eb9d43fef11f45487a802f187ea'}}).then(response => response.json())
-        .then(users => users.map(user => user.type))
+function acceptsGitHubUserName(username = "jameskevinayoung"){
+
+    return fetch(`https://api.github.com/users/${username}/events/`, {
+        headers: {'Authorization': `token ${githubAccessToken}`}})
+        .then(response => response.json())
+        .then(data => {
+            for(event of data) {
+                if(event.type === 'PushEvent'){
+                    let l = event.payload.commits.length;
+                    return event.payload.commits[l - 1].url;
+                }
+            }
+        })
+        .then(url =>{
+            console.log(url);
+            return (fetch(url, {headers:{"Authroization": `token ${githubAccessToken}`}})
+                    .then(response = response.json())
+                    .then(data => data.commit.committer.date)
+            );
+        })
 
 }
 
-
-console.log(acceptsGitHubUserName())
-
+acceptsGitHubUserName().then(data =>data).then(date => console.log(date));
