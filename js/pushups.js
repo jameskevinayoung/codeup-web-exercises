1efbe8bb5840e3c5f39ec9beb02c5b01fa7117a3
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-09-13 09:28:31 -0500

    updated files

--- js/pushups.js
+++ js/pushups.js
@@ -525,33 +525,44 @@ console.log(getFibonacci(10));
 // console.log(" is prime? " + isPrime(1234567891234567891));
 
 
-const movies =[
-    {
-        name: 'Hercules',
-        price: 3.50
-    },
-    {
-        name: 'Little Mermaid',
-        price: 5.00
-    },
-    {
-        name: 'Brother Bear',
-        price: 7.50
-    }
-]
+// const movies =[
+//     {
+//         name: 'Hercules',
+//         price: 3.50
+//     },
+//     {
+//         name: 'Little Mermaid',
+//         price: 5.00
+//     },
+//     {
+//         name: 'Brother Bear',
+//         price: 7.50
+//     }
+// ]
+//
+// function totalCost(array,day){
+//     let priceTotal = 0;
+//
+//     array.forEach(function(movie){
+//         priceTotal += movie.price * day;
+//
+//     });
+//     return priceTotal;
+//
+// }
+//
+// console.log(totalCost(movies, 5));
 
-function totalCost(array,day){
-    let priceTotal = 0;
 
-    array.forEach(function(movie){
-        priceTotal += movie.price * day;
+// CSS Color Names
+// Compiled by @bobspace.
+//
+// A javascript array containing all of the color names listed in the CSS Spec.
+// The full list can be found here: http://www.w3schools.com/cssref/css_colornames.asp
+// Use it as you please, 'cuz you can't, like, own a color, man.
 
-    });
-    return priceTotal;
-
-}
-
-console.log(totalCost(movies, 5));
+var CSS_COLOR_NAMES = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
 
 
 
+Collapse 
